8ee1c1190d6fd154fcc34dc8213bdcc01fb601bf
Lucas Cordeiro <105241465+IoStracciatella@users.noreply.github.com>
2026-03-13 23:17:51 -0300

    codigo que calcula a formula de bhaskarar

---
+++ bhaskara.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+    double a, b, c, x1, x2;
+    double delta = 0;
+
+    scanf("%lf %lf %lf", &a, &b, &c);
+
+     if (a == 0) {
+        printf("nao eh uma equacao do segundo grau");
+        return 0;
+    }
+
+    delta = (b*b) - 4*a*c;
+
+    if (delta > 0) {
+
+        x1 = (-b + sqrt(delta))/(2 * a);
+        x2 = (-b - sqrt(delta))/(2 * a);
+
+        printf("x1 = %.5lf\nx2 = %.5lf", x1, x2);
+
+    } else if (delta == 0) {
+
+         x1 = -b/(2 * a);
+        printf("Existe apenas um valor x = %.5lf que satisfaz a equacao", x1);
+
+    } else if (delta < 0) {
+
+        printf("nao existe x real que satisfaca a equacao");
+
+    }
+
+    return 0;
+}
