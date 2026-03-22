6294320638e2f8dd102755b1a9836d1bc1be2c83
Lucas Cordeiro <105241465+IoStracciatella@users.noreply.github.com>
2026-03-22 02:54:10 -0300

    corrigindo a falta de verificação das somas das colunas

--- quadrado_magico.c
+++ quadrado_magico.c
@@ -1,3 +1,8 @@
+//Um quadrado mágico é uma matriz quadrada em que a soma de todas as linhas e de todas as colunas é igual. Exemplo: Se eu somar
+//todos os elementos de uma linha i, o resultad será igual a somar todos os elementos de uma coluna j.
+
+//Para implementar isso no código
+
 #include <stdio.h>
 
 int main() {
@@ -17,6 +22,21 @@ int main() {
         
         for (int j = 0; j < 3; j++) {
             soma += matriz[i][j];
+        }
+
+        if (soma != soma_aux && i > 0) {
+            printf("nao eh quadrado magico");
+            return 0;
+        } else { 
+            soma_aux = soma;
+            soma = 0;
+        }
+    }
+
+    for (int i = 0; i < 3; i++) {
+        
+        for (int j = 0; j < 3; j++) {
+            soma += matriz[j][i];
         }
 
         if (soma != soma_aux && i > 0) {
