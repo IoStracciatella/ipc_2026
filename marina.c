cab49b770c01e618ec3b7f10eee0bd2d5a8f4621
Lucas Cordeiro <105241465+IoStracciatella@users.noreply.github.com>
2026-04-07 22:51:57 -0300

    comparando chars e ints

--- marina.c
+++ marina.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
 
 int main() {
+  int a;
+  scanf("%d", &a);
 
+  char b;
+  scanf("%c", &b);
+
+  if (a == b) {
+    printf("veja, chars sao valores numericos assim como ints");
+  
   return 0;
 }
