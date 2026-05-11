80034c69482e4ba66cefe62b5436695625568918
Lucas Cordeiro <105241465+IoStracciatella@users.noreply.github.com>
2026-05-11 01:57:09 -0300

    soma de riemann

--- riemann.c
+++ riemann.c
@@ -1,6 +1,8 @@
 // Fiz esse código pelo celular
+// No loop for é n+2 pra corrigir o fato que o < faz ele parar 2 índice antes do que deveria, porque na matemática o somatório da soma de Riemann vai de 0 até n inclusive, e como aqui a gente usa < e começa e termina o índice em n-1, a gentr tem que corrigir
 
-<stdio.h>
+
+#include <stdio.h>
 
 int main() {
     double p, q, n, a, b, c, delta, x, y, soma;
@@ -14,7 +16,7 @@ int main() {
     delta = q - p;
     delta = delta/n;
     
-    for (int i = 0; i < n+2; i++) { // Aqui é n+2 pra corrigir o fato que o < faz ele parar 2 índice antes do que deveria, porque na matemática o somatório da soma de Riemann vai de 0 até n inclusive, e como aqui a gente usa < e começa e termina o índice em n-1, a gentr tem que corrigir
+    for (int i = 0; i < n+2; i++) {
         x += delta;
         y = a*(x*x) + b*x + c;
         y = y*delta;
