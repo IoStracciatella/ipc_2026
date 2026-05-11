ce0b755d50d17cde2abb2dc59468b2f1e577da71
Lucas Cordeiro <105241465+IoStracciatella@users.noreply.github.com>
2026-05-10 23:40:11 -0300

    programa que calcula a soma de riemann

---
+++ riemann.c
@@ -0,0 +1,28 @@
+// Fiz esse código pelo celular
+
+<stdio.h>
+
+int main() {
+    double p, q, n, a, b, c, delta, x, y, soma;
+    
+    soma = 0;
+    
+    scanf("%lf %lf %lf", &a, &b, &c);
+    scanf("%lf %lf", &p, &q);
+    scanf("%lf", &n);
+    
+    delta = q - p;
+    delta = delta/n;
+    
+    for (int i = 0; i < n+2; i++) { // Aqui é n+2 pra corrigir o fato que o < faz ele parar 2 índice antes do que deveria, porque na matemática o somatório da soma de Riemann vai de 0 até n inclusive, e como aqui a gente usa < e começa e termina o índice em n-1, a gentr tem que corrigir
+        x += delta;
+        y = a*(x*x) + b*x + c;
+        y = y*delta;
+        
+        soma += y;
+    }
+    
+    printf("%lf", soma);
+
+    return 0;
+}
