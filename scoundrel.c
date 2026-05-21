8c65f524bd692c464f9b786abab3895e946d5437
Lucas Cordeiro <105241465+IoStracciatella@users.noreply.github.com>
2026-05-20 23:59:29 -0300

    Update scoundrel.c

--- scoundrel.c
+++ scoundrel.c
@@ -10,7 +10,7 @@ int main() {
     struct carta cartas[44];
     int indice_carta = 0, cont = 15;
 
-    //=====
+    //=====sdfsdfsdfsdfsdf
 
     for (int i = 0; i < 4; i++) {
         if (i > 1) {
