f4f114ee8acce71fc8a6a80c9e16b259f2df34a4
Lucas Cordeiro <105241465+IoStracciatella@users.noreply.github.com>
2026-06-09 01:46:48 -0300

    programa que converte um número decimal para algarismos romanos

---
+++ SPQR.c
@@ -0,0 +1,79 @@
+#include <stdio.h>
+
+int main() {
+    int numero, indice = 0;
+    char letras[50];
+
+    printf("Informe um numero decimal: ");
+    scanf("%d", &numero);
+
+    while (numero != 0) {
+        if ((numero / 1000) > 0) {
+            numero -= 1000;
+            letras[indice] = 'M';
+            indice++;
+        } else if ((numero / 500) > 0) {
+            numero -= 500;
+            letras[indice] = 'D';
+            indice++;
+        } else if ((numero / 100) > 0) {
+            numero -= 100;
+            letras[indice] = 'C';
+            indice++;
+        } else if ((numero / 50) > 0) {
+            numero -= 50;
+            letras[indice] = 'L';
+            indice++;
+        } else if ((numero / 10) > 0) {
+            numero -= 10;
+            letras[indice] = 'X';
+            indice++;
+        } else if (numero < 10) {
+            if (numero == 1) {
+                letras[indice] = 'I';
+            } else if (numero == 2) {
+                letras[indice] = 'I';
+                indice++;
+                letras[indice] = 'I';
+            } else if (numero == 3) {
+                letras[indice] = 'I';
+                indice++;
+                letras[indice] = 'I';
+                indice++;
+                letras[indice] = 'I';
+            } else if (numero == 4) {
+                letras[indice] = 'I';
+                indice++;
+                letras[indice] = 'V';
+            } else if (numero == 5) {
+                letras[indice] = 'V';
+            } else if (numero == 6) {
+                letras[indice] = 'V';
+                indice++;
+                letras[indice] = 'I';
+            } else if (numero == 7) {
+                letras[indice] = 'V';
+                indice++;
+                letras[indice] = 'I';
+                indice++;
+                letras[indice] = 'I';
+            } else if (numero == 8) {
+                letras[indice] = 'I';
+                indice++;
+                letras[indice] = 'I';
+                indice++;
+                letras[indice] = 'X';
+            } else if (numero == 9) {
+                letras[indice] = 'I';
+                indice++;
+                letras[indice] = 'X';
+            }
+
+            numero = 0;
+        }
+    }
+
+    printf("O numero romano correspondente eh: %s", letras);
+
+    return 0;
+}
