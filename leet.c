7e85e013e44635a367eb9fc96ed6f98f2eae9cc6
Lucas Cordeiro <105241465+IoStracciatella@users.noreply.github.com>
2026-06-13 01:52:52 -0300

    programa que transforma uma string em 1337

--- leet.c
+++ leet.c
@@ -1,7 +1,4 @@
 // O erro no código era o "&" no primeiro scanf, porque v já é um ponteiro
-// Acho que os casos teste do runcodes estão errados. Porque o padrão no leet é que os caracteres que não são
-// substituídos por números sejam colocados em caixa alta, e no runcodes eles tão em caixa baixa. De qualquer
-// jeito, eu comentei o default do break que faz os caracteres ficarem em caixa alta, pra passar nos casos teste
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -34,9 +31,9 @@ int main() {
                 v[i] = '0';
                 break;
 
-            /*default:
+            default:
                 v[i] = toupper(v[i]);
-                break;*/
+                break;
         }
     }
 
